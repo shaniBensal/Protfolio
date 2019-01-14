d764ec02330cac2d577e3ca536afd72572e912af
Shani <shano.bensal@mail.huji.ac.il>
2019-01-15 00:06:28 +0200

    Updates

---
+++ precache-manifest.1c1b2d8664c1bb56921b724520db6d12.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c46848f34aa4fc0a1ba109fc95d025d",
+    "url": "/my-protfolio/static/media/smallHeaderImg.2c46848f.jpg"
+  },
+  {
+    "revision": "c3ac11da2a81605bc5ed",
+    "url": "/my-protfolio/static/css/main.87db79a2.chunk.css"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/my-protfolio/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "8496b5be6b1d203aaf8b",
+    "url": "/my-protfolio/static/js/1.8496b5be.chunk.js"
+  },
+  {
+    "revision": "0c51ce8bdaa598589d4e",
+    "url": "/my-protfolio/static/js/runtime~main.0c51ce8b.js"
+  },
+  {
+    "revision": "f4cce7818c0807122d411dcef251a12d",
+    "url": "/my-protfolio/static/media/myPic.f4cce781.jpg"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/my-protfolio/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/my-protfolio/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/my-protfolio/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "77f7a0414a3b717e42045139144dc3b7",
+    "url": "/my-protfolio/static/media/appSus.77f7a041.jpg"
+  },
+  {
+    "revision": "2cd24eec7b15a1d66d4950ff72e12d67",
+    "url": "/my-protfolio/static/media/memeGenerator.2cd24eec.jpg"
+  },
+  {
+    "revision": "f831dc2ba0ac8b1eb7f751e4434164ac",
+    "url": "/my-protfolio/static/media/misterBit.f831dc2b.jpg"
+  },
+  {
+    "revision": "e7be1ebbe361c1fc148c1ce11b7dff44",
+    "url": "/my-protfolio/static/media/myCinema.e7be1ebb.jpg"
+  },
+  {
+    "revision": "a2d81aeca1343c8958eb83efbefa9702",
+    "url": "/my-protfolio/static/media/myLocations.a2d81aec.jpg"
+  },
+  {
+    "revision": "be456c1f524500c667b8b3758101b049",
+    "url": "/my-protfolio/static/media/psdToHtml.be456c1f.jpg"
+  },
+  {
+    "revision": "437428375f70ad9a604ddf022be97002",
+    "url": "/my-protfolio/static/media/rentApp.43742837.jpg"
+  },
+  {
+    "revision": "1b04684db7e587c363c5f77c24e8004e",
+    "url": "/my-protfolio/static/media/wikiTube.1b04684d.jpg"
+  },
+  {
+    "revision": "c3ac11da2a81605bc5ed",
+    "url": "/my-protfolio/static/js/main.c3ac11da.chunk.js"
+  },
+  {
+    "revision": "50c32d5f28304bb5250c1d82a2839695",
+    "url": "/my-protfolio/static/media/meGeorgia.50c32d5f.JPG"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/my-protfolio/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/my-protfolio/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/my-protfolio/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/my-protfolio/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/my-protfolio/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/my-protfolio/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/my-protfolio/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/my-protfolio/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/my-protfolio/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/my-protfolio/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/my-protfolio/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/my-protfolio/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/my-protfolio/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/my-protfolio/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "8496b5be6b1d203aaf8b",
+    "url": "/my-protfolio/static/css/1.2e9accca.chunk.css"
+  },
+  {
+    "revision": "85a5fe95289a5eba74ddd47158664c47",
+    "url": "/my-protfolio/index.html"
+  }
+];
